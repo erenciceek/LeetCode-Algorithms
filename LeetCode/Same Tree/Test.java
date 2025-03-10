5b09fa01273b64cfa156a11b1f160c6bcf362a1d
“erenciceek“ <eren.cicek62@gmail.com>
2025-03-10 14:37:57 +0300

    a question is added

---
+++ LeetCode/Same Tree/Test.java
@@ -0,0 +1,50 @@
+public class Test {
+    public static void main(String[] args) {
+        // same trees
+        TreeNode p1 = new TreeNode(1, new TreeNode(2), new TreeNode(3));
+        TreeNode q1 = new TreeNode(1, new TreeNode(2), new TreeNode(3));
+        System.out.println("Test 1 - Expected: true, Result: " + Solution.isSameTree(p1, q1));
+
+        // different trees
+        TreeNode p2 = new TreeNode(1, new TreeNode(2), null);
+        TreeNode q2 = new TreeNode(1, null, new TreeNode(2));
+        System.out.println("Test 2 - Expected: false, Result: " + Solution.isSameTree(p2, q2));
+
+    }
+}
+
+class Solution {
+    public static boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null) {
+            return true;
+        }
+
+        if (p == null || q == null) {
+            return false;
+        }
+
+        if (p.val != q.val) {
+            return false;
+        }
+
+        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    }
+}
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {}
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
