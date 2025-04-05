55373f80f7f02926038fe9ea711bbf521b0b9ccb
“erenciceek“ <eren.cicek62@gmail.com>
2025-04-05 20:08:34 +0300

    a question is added

---
+++ LeetCode/Implement Queue using Stacks/Test.java
@@ -0,0 +1,60 @@
+import java.util.Stack;
+
+public class Test {
+    public static void main(String[] args) {
+        MyQueue myQueue = new MyQueue();
+        myQueue.push(1); // queue is: [1]
+        myQueue.push(2); // queue is: [1, 2] (leftmost is front of the queue)
+        myQueue.peek(); // return 1
+        myQueue.pop(); // return 1, queue is [2]
+        myQueue.empty(); // return false
+    }
+}
+
+
+class MyQueue {
+    private Stack<Integer> s1;
+    private Stack<Integer> s2;
+
+    public MyQueue() {
+        s1 = new Stack<>();
+        s2 = new Stack<>();
+    }
+
+    public void push(int x) {
+        while (!s1.isEmpty()) {
+            s2.push(s1.pop());
+        }
+        s1.push(x);
+        while (!s2.isEmpty()) {
+            s1.push(s2.pop());
+        }
+    }
+
+    public int pop() {
+        if (s1.isEmpty()) {
+            return -1;
+        }
+        return s1.pop();
+    }
+
+    public int peek() {
+        if (s1.isEmpty()) {
+            return -1;
+        }
+        return s1.peek();
+    }
+
+    public boolean empty() {
+        return s1.isEmpty();
+    }
+}
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * MyQueue obj = new MyQueue();
+ * obj.push(x);
+ * int param_2 = obj.pop();
+ * int param_3 = obj.peek();
+ * boolean param_4 = obj.empty();
+ */
