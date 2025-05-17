fcc732d2ff4d44b9479927a45372a9735f503e37
“erenciceek“ <eren.cicek62@gmail.com>
2025-05-17 13:39:16 +0300

    A question is added

---
+++ LeetCode/Find First and Last Position of Element in Sorted Array/Test.java
@@ -0,0 +1,41 @@
+import java.util.Arrays;
+
+public class Test {
+    public static void main(String[] args) {
+        int[] nums = {5,7,7,8,8,10};
+        System.out.println(Arrays.toString(Solution.searchRange(nums,8)));
+    }
+}
+
+class Solution {
+    public static int[] searchRange(int[] nums, int target) {
+        int first = findBound(nums, target, true);
+        int last = findBound(nums, target, false);
+        return new int[]{first, last};
+    }
+
+
+    private static int findBound(int[] nums, int target, boolean isFirst) {
+        int left = 0;
+        int right = nums.length - 1;
+        int result = -1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] == target) {
+                result = mid;
+                if (isFirst) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return result;
+    }
+}
